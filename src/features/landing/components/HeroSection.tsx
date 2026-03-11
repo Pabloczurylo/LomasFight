8093665e0c079749795e88f459dc70a7e972f4be
maximiliano <juanmaximilianoprieto@hotmail.com>
2026-03-11 12:46:18 -0300

    chuscha

--- src/features/landing/components/HeroSection.tsx
+++ src/features/landing/components/HeroSection.tsx
@@ -11,7 +11,7 @@ export function HeroSection() {
             <div className="absolute inset-0 z-0">
                 <div className="absolute inset-0 bg-gradient-to-t from-black via-black/50 to-transparent z-10" />
                 <img
-                    src="https://i.imgur.com/QIrXU7o.png"
+                    src="https://i.imgur.com/SMMttMP.png"
                     alt="Lomas Fight Gym"
                     className="w-full h-full object-cover opacity-60"
                 />
@@ -20,7 +20,7 @@ export function HeroSection() {
             <div className="relative z-20 container mx-auto px-4 text-center flex flex-col items-center">
                 <h1 className="text-4xl sm:text-6xl md:text-8xl lg:text-9xl font-heading font-bold text-white uppercase leading-[0.9] mb-6">
                     BIENVENIDOS A <br />
-                    <span className="text-brand-red">CHUCHA</span>
+                    <span className="text-brand-red">CHUSCHA</span>
                 </h1>
 
                 <p className="max-w-xl text-gray-300 text-lg md:text-xl mb-8 font-body font-light">
